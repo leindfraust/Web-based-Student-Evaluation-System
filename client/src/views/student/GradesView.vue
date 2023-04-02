c403f0016f6dd4f07012f31caa99e8c7f057a3fa
Leindfraust <rozer223@gmail.com>
2023-04-02 14:12:36 +0800

    feat: added grade view

--- client/src/views/student/GradesView.vue
+++ client/src/views/student/GradesView.vue
@@ -1,16 +1,61 @@
 <script lang="ts" setup>
 import NavPanel from '@/components/NavPanel.vue';
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+import { useCredentialsStore } from '@/stores/CredentialInformation';
+import CatchErr from '@/components/CatchErr.vue';
+
+const store = useCredentialsStore()
+
+const handlerInfo = ref(store.$state.credentials as unknown as Record<string, string>)
+const handlerID = ref(handlerInfo.value.name) //student ID Number
+
+const studentSubjectsEnrolled = ref()
+
+const errCode = ref('')
+const errMsg = ref('')
+
+onMounted(async () => {
+    await axios.post(`/api/student-records/inquire`, {
+        id: handlerID.value
+    }).then(response => studentSubjectsEnrolled.value = response.data.studentSubjectsEnrolled).catch(err => {
+        errCode.value = err.code
+        errMsg.value = err.message
+    })
+})
 
 </script>
 <template>
-    <div class="main-wrapper">
+    <div class="">
         <div class="columns">
             <div class="column is-2">
                 <NavPanel />
             </div>
             <div class="column">
                 <section class="section">
-                    <h1 class="title is-1 notification is-link">This section is still in development</h1>
+                    <div class="container">
+                        <CatchErr :err-code="errCode" :err-message="errMsg" />
+                        <table class="table-container">
+                            <div class="table is-fullwidth">
+                                <thead>
+                                    <tr>
+                                        <th>Subject Code</th>
+                                        <th>Subject Description</th>
+                                        <th>Grade</th>
+                                        <th>Instructor</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="subject, index in studentSubjectsEnrolled" :key="index">
+                                        <th>{{ subject.code }}</th>
+                                        <td>{{ subject.description }}</td>
+                                        <td>{{ subject.grade }}</td>
+                                        <td>{{ subject.instructor }}</td>
+                                    </tr>
+                                </tbody>
+                            </div>
+                        </table>
+                    </div>
                 </section>
             </div>
         </div>
